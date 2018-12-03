3996f669e7fc89757279d16c1440d0ace87829e6
Allen Shiu <pt7922310@gmail.com>
2018-12-03 18:05:48 +0800

    Initial Commit

---
+++ LoadingAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingAnimation
+//
+//  Created by 許佳豪 on 2018/12/3.
+//  Copyright © 2018 許佳豪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
